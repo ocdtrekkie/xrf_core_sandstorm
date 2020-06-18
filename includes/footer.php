91e07983146faaaeaa43980a7c4d333c416c08c8
Jacob Weisz <inbox@jacobweisz.com>
2020-06-18 14:47:17 -0500

    Add default headers and styles
    Thanks, NASA, for the public domain image

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<?php
+echo "</div><div class=\"footer\" align=\"center\">
+<table width=\"100%\"><tr><td><font size=\"1\">&copy;2010-2020 Jacob Weisz</font></td></tr></table>
+</div></body></html>";
+mysqli_close($xrf_db);
+?>
---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/html; charset=iso-8859-15');
+if ($xrf_mystylepref == "") {$xrf_style = $xrf_style_default;}
+else {$xrf_style = $xrf_mystylepref;}
+echo "<html><head><title>$xrf_site_name</title>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/$xrf_style/style.css\" />
+</head><body>";
+
+echo "<div class=\"header\" align=\"center\">
+<table width=\"100%\"><tr><td>
+<p align=\"left\">
+<font size=\"6\">$xrf_site_name</font><br>
+$xrf_myusername ($xrf_myemail)
+</p>
+</td></tr></table>
+</div>
+
+<div class=\"container\" align=\"center\">";
+?>
