97d29c161aa1666f464f10817f443d120f352f95
Jacob Weisz <inbox@jacobweisz.com>
2022-07-09 15:54:13 -0500

    Re-add xrf_generate_password

--- includes/functions_auth.php
+++ includes/functions_auth.php
@@ -12,4 +12,20 @@ function xrf_check_auth_version($xrf_auth_version_page, $xrf_auth_version_db)
 		return true;
 }
 
+//Function xrf_generate_password
+//Use: Generates a random password.
+function xrf_generate_password($length)
+{
+	$password = "";
+	$possible = "2346789bcdfghjkmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ"; // Possible characters
+	$maxlength = strlen($possible);
+	$i = 0;
+	while ($i < $length) {
+		$char = substr($possible, mt_rand(0, $maxlength-1), 1);
+		$password .= $char;
+		$i++;
+	}
+	return ($password);
+}
+
 ?>
